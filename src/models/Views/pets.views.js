b81819bfe0fbf79db82c57c38a0b35263d3c8007
josecuela <jgcuelap@gmail.com>
2022-07-19 11:26:28 -0500

    ruta mascotas de fundacion

--- src/models/Views/pets.views.js
+++ src/models/Views/pets.views.js
@@ -6,6 +6,105 @@ import { City } from '../../models/City.js';
 import { Country } from '../../models/Country.js';
 import { ColorPet } from '../../models/Colorpet.js';
 
+export const findByUser = async (userId) => {
+  const pets = await Pets.findAll({
+    attributes: { exclude: ['breedId', 'typeId'] },
+    include: [
+      {
+        model: ColorPet,
+        attributes: ['nameColor'],
+      },
+      {
+        model: TypePet,
+        attributes: ['nameType'],
+      },
+      {
+        model: BreedPet,
+        attributes: ['nameBreed'],
+      },
+      {
+        model: User,
+        // attributes: ['address'],
+        include: [
+          {
+            model: Country,
+          },
+          {
+            model: City,
+            // attributes: ['name'],
+          }
+        ]
+      }
+    ],
+    where: {
+      userId: userId,
+    },
+    raw: true,
+  });
+
+  const responsePets = pets.map((pet) => {
+    pet.environment = JSON.parse(pet.environment)
+    pet.attributes = JSON.parse(pet.attributes)
+    pet.photos = pet.photos.map((photo) => {
+      return {
+        option_1: photo,
+        option_2: photo,
+        option_3: photo,
+      }
+    })
+
+    pet['type'] = pet["typepet.nameType"];
+    pet['breed'] = pet["breedpet.nameBreed"];
+    pet['color'] = pet["colorpet.nameColor"];
+    pet['role'] = pet["user.role"];
+    let date = new Date(pet["published_at"]);
+    pet.status_changed_at = Number(date); // EPOCH format
+    pet.contact = {
+      email: pet["user.email"],
+      phone: pet["user.phone"],
+      address: {
+        address: pet["user.address"],
+        city: pet["user.city.name"],
+        state: pet["user.state.name"] || null,
+        postcode: pet["user.city.postcode"] || null,
+        country: pet["user.countryId"]
+      }
+    }
+
+    delete pet["user.id"];
+    delete pet["user.name"];
+    delete pet["user.email"];
+    delete pet["user.phone"];
+    delete pet["user.lastName"];
+    delete pet["user.password"];
+    delete pet["user.address"];
+    delete pet["user.cityId"];
+    delete pet["user.city.name"];
+    delete pet["user.city.id"];
+    delete pet["user.city.countryId"];
+
+    delete pet["user.country.id"];
+    delete pet["user.country.name"];
+    delete pet["user.country.currency"];
+    delete pet["user.country.symbol"];
+
+    delete pet["user.countryId"];
+    delete pet["user.role"];
+    delete pet["user.active"];
+    delete pet["user.verification"];
+    delete pet["user.donaciones"];
+    delete pet["user.document"];
+
+    delete pet["colorpet.nameColor"];
+    delete pet["typepet.nameType"];
+    delete pet["breedpet.nameBreed"];
+    delete pet["colorId"];
+
+    return pet
+  });
+  return responsePets;
+}
+
 export const findAllPets = async () => {
   const pets = await Pets.findAll({
     attributes: { exclude: ['breedId', 'typeId'] },
