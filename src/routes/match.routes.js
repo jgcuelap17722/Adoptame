837a070f6f748a85471be34530299dba5a685a3a
Germanj18 <germnaguilera@gmail.com>
2022-07-22 17:09:17 -0300

    fix de match con delete para prueba

--- src/routes/match.routes.js
+++ src/routes/match.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { matchPet, PutMachUser } from "../controllers/match.Controller.js";
+import { DeleteMatch, matchPet, PutMachUser } from "../controllers/match.Controller.js";
 
 
 
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post('/',matchPet)
 router.put('/matchUser/:id',PutMachUser)
+router.delete('/delete/:id',DeleteMatch)
 
 
 
